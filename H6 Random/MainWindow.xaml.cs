f987cd0fd54dee6a8968d06187d85c396fd63c6f
cathoDr-immalle <catho.driesen@immalle.be>
2017-10-24 08:49:53 +0200

    Random age guess

--- H6 Random/MainWindow.xaml.cs
+++ H6 Random/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace H6_Random
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random ageGuesser = new Random();
+        private int tries = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            GuessLabel.Content = Convert.ToString(ageGuesser.Next(5, 110));
+        }
+
+        private void CorrectKnop_Click(object sender, RoutedEventArgs e)
+        {
+            tries = tries + 1;
+            MessageBox.Show("Number of tries was: " + tries);
+            tries = 0;
+            GuessLabel.Content = Convert.ToString(ageGuesser.Next(5, 110));
+        }
+
+        private void WrongKnop_Click(object sender, RoutedEventArgs e)
+        {
+            GuessLabel.Content = Convert.ToString(ageGuesser.Next(5, 110));
+            tries = tries + 1;
         }
     }
 }
